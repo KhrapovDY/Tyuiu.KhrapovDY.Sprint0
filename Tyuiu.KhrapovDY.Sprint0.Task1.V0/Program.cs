9b2eed54344ce485004bf197ca840617deb434c3
Daniil Khrapov <Tyuiu.KhrapovD@outlook.com>
2024-09-11 23:31:00 +0500

    Task1

---
+++ Tyuiu.KhrapovDY.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tyuiu.KhrapovD.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Даниил";
+            int age = 17;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadLine();
+        }
+    }
+}
