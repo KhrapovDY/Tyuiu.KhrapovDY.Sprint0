2103b5d13a7232a07272b0e92dfcf61e19631cdb
Daniil Khrapov <Tyuiu.KhrapovD@outlook.com>
2024-09-16 12:00:09 +0500

    Task3

---
+++ Tyuiu.KhrapovDY.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KhrapovDY.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
