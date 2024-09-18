ec201273873e7902d87806cffc3de2c497c536f4
Daniil Khrapov <Tyuiu.KhrapovD@outlook.com>
2024-09-18 11:07:01 +0500

    Task5

---
+++ Tyuiu.KhrapovDY.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+using Tyuiu.KhrapovDY.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KhrapovDY.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+
+        [TestMethod]
+        public void CheckedMultiplicationnValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedDivisoinValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
+    }
+}
---
+++ Tyuiu.KhrapovDY.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KhrapovDY.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KhrapovDY.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+
+            // Пример разветвляющейся структуры находится в библеотеке классов в методе Division
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+            Console.ReadKey();
+         }
+    }
+}
