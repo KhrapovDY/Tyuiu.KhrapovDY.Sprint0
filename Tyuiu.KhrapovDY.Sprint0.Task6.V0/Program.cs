73f57ace80ce8392e09d1ed7e46ecdbf06d3f620
Daniil Khrapov <Tyuiu.KhrapovD@outlook.com>
2024-09-18 11:57:59 +0500

    Task6

---
+++ Tyuiu.KhrapovDY.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.KhrapovDY.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KhrapovDY.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            // Пример циклической структуры находятся в библеотеки классов в методе AdditionArray
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+            // Пример циклической структуры находятся в библеотеки классов в методе SubtractionArray
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+
+            // Пример циклической структуры находятся в библеотеки классов в методе MultiplicationArray
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
